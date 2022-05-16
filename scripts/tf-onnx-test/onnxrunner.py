fc9a26f038a2d3528d40baa91c245c003883c51f
edurso <eric@casadurso.com>
2022-05-16 07:23:05 -0400

    update runner

--- scripts/tf-onnx-test/onnxrunner.py
+++ scripts/tf-onnx-test/onnxrunner.py
@@ -6,6 +6,7 @@ sys.path.append('../scripts')
 
 # imports
 import os
+from cv2 import imread
 from scripts import onnxutils as utils
 
 batch_size = 1
@@ -18,3 +19,4 @@ test_img = '../img/frame-321.jpg'
 os.system('trtexec --onnx={} --saveEngine=engine.trt  --explicitBatch --inputIOFormats=fp16:chw --outputIOFormats=fp16:chw --fp16'.format(model_name))
 
 trt_model = utils.ONNXClassifierWrapper("engine.trt", [batch_size, 1000], target_dtype=precision)
+print(trt_model.predict(imread(test_img))[0][:10])
