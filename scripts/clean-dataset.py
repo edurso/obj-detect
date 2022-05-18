2a4ac8018418a849d1b19165f82066d854329612
edurso <eric@casadurso.com>
2022-05-18 07:22:55 -0400

    add script to remove unlabeled files from dataset

---
+++ scripts/clean-dataset.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+
+def main():
+
+	parser = argparse.ArgumentParser(description="Partition dataset of images into training and testing sets",
+									 formatter_class=argparse.RawTextHelpFormatter)
+	parser.add_argument(
+		'-d', '--dir',
+		help='Path to the folder where the image dataset is stored. If not specified, the CWD will be used.',
+		type=str,
+		default=os.getcwd()
+	)
+	
+	args = parser.parse_args()
+	dir = args.dir
+	
+	files = os.listdir(dir)
+	removed = 0
+	for i in range(len(files)):
+		if os.path.splitext(files[i])[0] + '.xml' not in files:
+			os.remove(files[i])
+			removed += 1
+
+	print('Successfully removed {} unlabeled files'.format(removed))
+
+if __name__ == '__main__':
+	main()
+
